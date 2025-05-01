3c7e41d263260baeb762ad0436af28edee9f513c
Ruscyl <140474695+Ruscyl@users.noreply.github.com>
2025-05-02 04:03:44 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,222 +1,406 @@
 #include <iostream>
 #include <string>
-#include <math.h>
+#include <sstream>
+#include <vector>
+#include <cstring>
+#include <iterator>
+#include <algorithm>
+#include <fstream>
 using namespace std;
 
-string school_method_addition(string first_num, string second_num, int base)
+class Node
 {
+private:
+public:
+    Node();
+    Node(int value);
+    Node *parent;
+    Node *parent_right;
+    Node *left;
+    Node *right;
+    int value;
+    int hl;
+    int hr;
+    int height;
+    ~Node();
+};
 
-	int num1_len = first_num.size();
-	int num2_len = second_num.size();
-	int result_len = max(num1_len, num2_len) + 1; 
-	string result_str;
-	int carry = 0;
-	
-	if (num1_len < num2_len)
-	{
-		for (int i = 0; i < num2_len - num1_len; i++)
-		{
-			first_num = '0' + first_num;
-		}
-		num1_len = num2_len;
-	}
-	else if (num1_len > num2_len)
-	{
-		for (int i = 0; i < num1_len - num2_len; i++)
-		{
-			second_num = '0' + second_num;
-		}
-		num2_len = num1_len;
-	}
-
-	for (int i = 0; i < result_len - 1; i++)
-	{
-		int num1, num2, curr_result;
-		num1 = first_num[num1_len - 1 - i] - '0';
-		num2 = second_num[num2_len - 1 - i] - '0';
-		curr_result = num1 + num2 + carry;
-		int result = curr_result;
-		if (curr_result >= base)
-		{
-			carry = 1;
-			result = curr_result - base;
-		}
-		else
-		{
-			carry = 0;
-		}
-		
-		result_str = to_string(result) + result_str;
-	}
-	result_str = to_string(carry) + result_str;
-
-	int breakpoint = result_str.size();
-	for (int i = 0; i < breakpoint; i++)
-	{
-		if (result_str[i] != '0')
-		{
-			breakpoint = i;
-			break;
-		}
-	}
-
-	result_str = result_str.substr(breakpoint, result_str.size() - breakpoint);
-	if (result_str.size() == 0) {
-		return "0";
-	}
-	return result_str;
+Node::Node()
+{
+    value = -1;
+    left = nullptr;
+    right = nullptr;
+    parent = nullptr;
+    parent_right = nullptr;
+    hl = 0;
+    hr = 0;
+    height = 1;
 }
 
-string school_method_substraction(string first_num, string second_num, int base)
+Node::Node(int p_value)
 {
-
-	int num1_len = first_num.size();
-	int num2_len = second_num.size();
-	int result_len = max(num1_len, num2_len);
-	string result_str="";
-	int carry = 0;
-
-	if (num1_len < num2_len)
-	{
-		for (int i = 0; i < num2_len - num1_len; i++)
-		{
-			first_num = '0' + first_num;
-		}
-		num1_len = num2_len;
-	}
-	else if (num1_len > num2_len)
-	{
-		for (int i = 0; i < num1_len - num2_len; i++)
-		{
-			second_num = '0' + second_num;
-		}
-		num2_len = num1_len;
-	}
-
-	for (int i = 0; i < result_len; i++)
-	{
-		int num1, num2, curr_result;
-		num1 = first_num[num1_len - 1 - i] - '0';
-		num2 = second_num[num2_len - 1 - i] - '0';
-		num1 = num1 - carry;
-		if (num1 < num2)
-		{
-			carry = 1;
-			curr_result = base + num1 - num2;
-		}
-		else
-		{
-			carry = 0;
-			curr_result = num1 - num2;
-		}
-		result_str = to_string(curr_result) + result_str;
-	}
-
-	int breakpoint = result_str.size();
-	for (int i = 0; i < breakpoint; i++)
-	{
-		if (result_str[i] != '0')
-		{
-			breakpoint = i;
-			break;
-		}
-	}
-
-	result_str = result_str.substr(breakpoint, result_str.size() - breakpoint);
-	return result_str;
+    value = p_value;
+    left = nullptr;
+    right = nullptr;
+    parent = nullptr;
+    parent_right = nullptr;
+    hl = 0;
+    hr = 0;
+    height = 1;
 }
 
-string convertBase(int number, int base, string result)
+Node::~Node()
 {
-	if (number < base)
-	{
-		return to_string(number);
-	}
-	else
-	{
-		int x = number % base;
-		number = number / base;
-		result = convertBase(number, base, result);
-		result = result + to_string(x);
-	}
-	return result;
 }
 
-
-string karatsuba(string first_num, string second_num, int base)
+int get_height(Node *N)
 {
-
-	int strlen1 = first_num.size();
-	int strlen2 = second_num.size();
-	if (strlen1 < strlen2)
-	{
-		for (int i = 0; i < strlen2 - strlen1; i++)
-		{
-			first_num = '0' + first_num;
-		}
-	}
-	else if (strlen1 > strlen2)
-	{
-		for (int i = 0; i < strlen1 - strlen2; i++)
-		{
-			second_num = '0' + second_num;
-		}
-	}
-
-	if (first_num.size() == 1 && second_num.size() == 1)
-	{
-		int num1 = first_num[0] - '0';
-		int num2 = second_num[0] - '0';
-		int mul = num1 * num2;
-		string result = "";
-		result = convertBase(mul, base, result);
-		return result;
-	}
-	else
-	{
-		int mid = first_num.size() / 2;
-		string a1 = first_num.substr(0, mid);
-		string a0 = first_num.substr(mid, first_num.size() - mid);
-		string b1 = second_num.substr(0, mid);
-		string b0 = second_num.substr(mid, second_num.size() - mid);
-		int k = first_num.size() - mid;
-		
-		string a1b1 = karatsuba(a1, b1,base);
-		string a1_a0 = school_method_addition(a1, a0, base);
-		string b1_b0 = school_method_addition(b1, b0, base);
-		string a0b0 = karatsuba(a0, b0, base);
-
-		string part1 = a1b1;
-		for (int i =0;i<2*k;i++){
-			part1 =part1 +"0";
-		}
-
-		string part2 = karatsuba(a1_a0, b1_b0, base);
-		string part3 = school_method_addition(a1b1, a0b0,base);
-		string part_mid = school_method_substraction(part2,part3,base);
-		for (int i =0;i<k;i++){
-			part_mid =part_mid +"0";
-		}
-
-		string part4 = a0b0;
-
-		string result = part1;
-		result = school_method_addition(result,part_mid,base);
-		result = school_method_addition(result,part4,base);
-		return result;
-
-	}
-
-	return "0";
+    if (N != NULL)
+        return N->height;
+    return 0;
 }
 
+Node *rightRotate(Node *node)
+{
+    Node *left_node;
+    Node *leftRight_node;
+    left_node = node->left;
+    leftRight_node = left_node->right;
+    left_node->right = node;
+    node->left = leftRight_node;
 
+    node->height = max(get_height(node->left), get_height(node->right)) + 1; 
+    left_node->height = max(get_height(left_node->left), get_height(left_node->right)) + 1; 
+
+    return left_node;
+}
+
+Node *leftRotate(Node *node)
+{
+    Node *right_node;
+    Node *rightleft_node;
+    right_node = node->right;
+    rightleft_node = right_node->left;
+    right_node->left = node;
+    node->right = rightleft_node;
+
+    if (node->left == NULL && node->right == NULL)
+    {
+        node->height = 1;
+    }
+    else if (node->left == NULL)
+    {
+        node->height = node->right->height + 1;
+    }
+    else if (node->right == NULL)
+    {
+        node->height = node->left->height + 1;
+    }
+    else
+    {
+        node->height = max(node->left->height, node->right->height) + 1;
+    }
+
+    if (right_node->left == NULL && right_node->right == NULL)
+    {
+        right_node->height = 1;
+    }
+    else if (right_node->left == NULL)
+    {
+        right_node->height = right_node->right->height + 1;
+    }
+    else if (right_node->right == NULL)
+    {
+        right_node->height = right_node->left->height + 1;
+    }
+    else
+    {
+        right_node->height = max(right_node->left->height, right_node->right->height) + 1;
+    }
+    return right_node;
+}
+
+int getBalance(Node *N)
+{
+    int left;
+    int right;
+    if (N->left == NULL)
+    {
+        left = 0;
+    }
+    else
+    {
+        left = N->left->height;
+    }
+
+    if (N->right == NULL)
+    {
+        right = 0;
+    }
+    else
+    {
+        right = N->right->height;
+    }
+    return left - right;
+}
+
+Node *addNode(Node *node, int value)
+{
+    Node *n = new Node(value);
+    if (node == NULL)
+        return (n);
+
+    if (value < node->value)
+        node->left = addNode(node->left, value);
+    else if (value > node->value)
+        node->right = addNode(node->right, value);
+    else
+        return node;
+
+    int left;
+    int right;
+    if (node->left == NULL)
+    {
+        left = 0;
+    }
+    else
+    {
+        left = node->left->height;
+    }
+
+    if (node->right == NULL)
+    {
+        right = 0;
+    }
+    else
+    {
+        right = node->right->height;
+    }
+    node->height = 1 + max(left, right);
+
+    int balance = getBalance(node);
+
+    if (balance > 1)
+    {
+        if (value < node->left->value)
+        {
+            return rightRotate(node);
+        }
+        if (value > node->left->value)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+    }
+    if (balance < -1)
+    {
+        if (value > node->right->value)
+        {
+            return leftRotate(node);
+        }
+        if (value < node->right->value)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+    }
+    return node;
+}
+
+Node *leftMax(Node *node)
+{
+    Node *current = node;
+    while (current->right != NULL)
+        current = current->right;
+
+    return current;
+}
+
+Node *deleteNode(Node *node, int value)
+{
+    if (node == NULL)
+        return node;
+    if (value < node->value)
+    {
+        node->left = deleteNode(node->left, value);
+    }
+    else if (value > node->value)
+    {
+        node->right = deleteNode(node->right, value);
+    }
+    else
+    {
+        if (node->left == NULL || node->right == NULL)
+        {
+            if (node->left == NULL && node->right == NULL)
+            {
+                node = NULL;
+            }
+            else if (node->left == NULL)
+            {
+                node = node->right;
+            }
+            else
+            {
+                node = node->left;
+            }
+        }
+        else
+        {
+            Node *temp = leftMax(node->left);
+            node->value = temp->value;
+            node->left = deleteNode(node->left, temp->value);
+        }
+    }
+
+    if (node == NULL)
+    {
+        return node;
+    }
+
+    if (node->left == NULL && node->right == NULL)
+    {
+        node->height = 1;
+    }
+    else if (node->left == NULL)
+    {
+        node->height = node->right->height + 1;
+    }
+    else if (node->right == NULL)
+    {
+        node->height = node->left->height + 1;
+    }
+    else
+    {
+        node->height = max(node->left->height, node->right->height) + 1;
+    }
+
+    int balance = getBalance(node);
+
+    if (balance > 1)
+    {
+        if (getBalance(node->left) >= 0)
+        {
+            return rightRotate(node);
+        }
+        if (getBalance(node->left) < 0)
+        {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+    }
+    if (balance < -1)
+    {
+        if (getBalance(node->right) <= 0)
+        {
+            return leftRotate(node);
+        }
+        if (getBalance(node->right) > 0)
+        {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+    }
+
+    return node;
+}
+
+void preOrder(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->value << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void inOrder(Node *node)
+{
+    if (node != NULL)
+    {
+        inOrder(node->left);
+        cout << node->value << " ";
+        inOrder(node->right);
+    }
+}
+
+void postOrder(Node* node){
+	if(node!= NULL){
+		postOrder(node->left);
+		postOrder(node->right);
+		cout << node->value << " ";
+	}  
+}
 
 int main()
 {
-	string first_num, second_num, base;
-	cin >> first_num >> second_num >> base;
-	int base_int = stoi(base);
-	cout << school_method_addition(first_num, second_num, base_int)<<" "<<karatsuba(first_num, second_num, base_int)<<endl;
-	return 0;
+
+    Node *root = NULL;
+    string input;
+    int number;
+    bool flag = true;
+
+    string out_type = " ";
+    while (flag)
+    {
+        cin >> input;
+        if (input[0] == 'P' || input[0] == 'I')
+        {
+            if (input == "PRE")
+            {
+                if (root == NULL)
+                {
+                    cout << "EMPTY";
+                    break;
+                }
+                else
+                {
+                    preOrder(root);
+                    break;
+                }
+                flag = false;
+            }
+            else if (input == "IN")
+            {
+                if (root == NULL)
+                {
+                    cout << "EMPTY";
+                    break;
+                }
+                else
+                {
+                    inOrder(root);
+                    break;
+                }
+                flag = false;
+            }
+            else if (input == "POST" )
+            {
+                if (root == NULL)
+                {
+                    cout << "EMPTY";
+                    break;
+                }
+                else
+                {
+                    postOrder(root);
+                    break;
+                }
+                flag = false;
+            }
+        }
+
+        if (input[0] == 'A')
+        {
+            number = stoi(input.substr(1));
+            root = addNode(root, number);
+        }
+        else if (input[0] == 'D')
+        {
+            number = stoi(input.substr(1));
+            root = deleteNode(root, number);
+        }
+    }
+    return 0;
 }
