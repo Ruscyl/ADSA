1f4c0fb560e3cbdaa53a511f7d1869e9833f8e45
Ruscyl <140474695+Ruscyl@users.noreply.github.com>
2025-05-10 12:07:24 +0930

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 def letter_to_cost(ch):
-    if 'A' <= ch <= 'Z':
-        return ord(ch) - ord('A')
-    else:
-        return ord(ch) - ord('a') + 26
+    return ord(ch) - ord('A') if 'A' <= ch <= 'Z' else ord(ch) - ord('a') + 26
 
 class DSU:
     def __init__(self, n):
@@ -14,9 +11,9 @@ class DSU:
         return self.parent[x]
 
     def union(self, x, y):
-        xroot, yroot = self.find(x), self.find(y)
-        if xroot != yroot:
-            self.parent[xroot] = yroot
+        xr, yr = self.find(x), self.find(y)
+        if xr != yr:
+            self.parent[xr] = yr
             return True
         return False
 
@@ -28,45 +25,28 @@ def main():
     destroy = destroy_str.split(',')
     n = len(country)
 
-    dsu = DSU(n)
     edges = []
+    destroy_total = 0
 
     for i in range(n):
         for j in range(i + 1, n):
             if country[i][j] == '1':
                 cost = letter_to_cost(destroy[i][j])
-                edges.append((0, i, j, 'keep', cost))  
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            if country[i][j] == '0':
+                destroy_total += cost
+                edges.append((-cost, i, j))
+            else:
                 cost = letter_to_cost(build[i][j])
-                edges.append((cost, i, j, 'build'))
+                edges.append((cost, i, j))
 
     edges.sort()
+    dsu = DSU(n)
+    savings = 0
 
-    used_edges = set()
-    total_cost = 0
-
-    for edge in edges:
-        if len(edge) == 5:
-            cost, u, v, typ, destroy_cost = edge
-        else:
-            cost, u, v, typ = edge
-            destroy_cost = None
-
+    for cost, u, v in edges:
         if dsu.union(u, v):
-            if typ == 'build':
-                total_cost += cost
-            elif typ == 'keep':
-                used_edges.add((u, v))
+            savings += -cost if cost < 0 else -cost 
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            if country[i][j] == '1' and (i, j) not in used_edges:
-                total_cost += letter_to_cost(destroy[i][j])
-
-    print(total_cost)
+    print(destroy_total + savings)
 
 if __name__ == "__main__":
     main()
